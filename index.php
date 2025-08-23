964aa4a1f46681376f050dcf3d6c26a6905c44ff
depaa81 <xiyennnyennn@gmail.com>
2025-08-24 00:19:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<h2>Selamat datang, <?php echo $_SESSION['username']; ?>!</h2>
+<p>Ini adalah halaman utama setelah login.</p>
+<a href="logout.php">Logout</a>
 
